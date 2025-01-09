fd20739843f81dc805d39772da09621edb86a19f
Brandon Simoncic <brandonsimoncic@gmail.com>
2025-01-09 14:58:18 -0500

    moved correct port

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ async fn main() {
 
     let routes = routes::routes();
     println!("Server started at http://localhost:8000");
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 10000)).await;
 }
